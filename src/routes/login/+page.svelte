4ba3c9b5469ac0a2521cf5728c589802727a3bd6
LolzTheDev <lolzthedev@gmail.com>
2024-11-06 21:52:46 -0600

    :lipstick: add input labels

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -23,8 +23,8 @@
   <h1 class="text-3xl font-bold">login</h1>
 
   <form on:submit={login} class="grid grid-cols-1 gap-2 w-64">
-    <input type="text" bind:value={username} />
-    <input type="password" bind:value={password} />
+    <input type="text" placeholder="username" bind:value={username} />
+    <input type="password" placeholder="password" bind:value={password} />
 
     <input type="submit" value="login" />
   </form>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -25,9 +25,9 @@
   <h1 class="text-3xl font-bold">register</h1>
 
   <form on:submit={register} class="grid grid-cols-1 gap-4 w-64">
-    <input type="text" bind:value={username} />
-    <input type="email" bind:value={email} />
-    <input type="password" bind:value={password} />
+    <input type="text" placeholder="username" bind:value={username} />
+    <input type="email" placeholder="email" bind:value={email} />
+    <input type="password" placeholder="password" bind:value={password} />
 
     <input type="submit" value="register" />
   </form>
