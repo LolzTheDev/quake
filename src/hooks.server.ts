a7d7fb0ee3de44ed57e9aad1238b217c905e699d
LolzTheDev <lolzthedev@gmail.com>
2024-11-12 22:17:45 -0600

    :sparkles: advanced logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,13 @@
 import { auth } from "$lib/jwt.server";
-import { redirect, type Handle } from "@sveltejs/kit";
+import { redirect, type Handle, type RequestEvent } from "@sveltejs/kit";
+import type { ResolveHookContext } from "module";
 
 const PROTECTED = ["/feed", "/post", "/settings"];
 const LI_PROTECTED = ["/login", "/register"];
 const MW_DISABLED = ["/api/logout"];
+const LOGS_BLACKLIST = ["/api/pfp", "/api/user"];
 
 export const handle: Handle = async ({ event, resolve }) => {
-  if (MW_DISABLED.includes(event.url.pathname)) return resolve(event);
-
   const authenticated = await auth.verifyToken(
     event.cookies.get("token") ?? "",
   );
@@ -22,6 +22,13 @@ export const handle: Handle = async ({ event, resolve }) => {
       event.cookies.get("token") ?? "",
     );
   }
+
+  !LOGS_BLACKLIST.some((path) => event.url.pathname.startsWith(path))
+    ? console.log(
+        `[${event.request.method}] ${event.url.pathname} - ${event.getClientAddress()} - @${event.locals.user.payload?.user ?? "[logged out]"}`,
+      )
+    : "";
+  if (MW_DISABLED.includes(event.url.pathname)) return resolve(event);
 
   if (!authenticated && PROTECTED.includes(event.url.pathname)) {
     throw redirect(303, "/login");
